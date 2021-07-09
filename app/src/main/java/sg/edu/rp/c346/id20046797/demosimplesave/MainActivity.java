4b465af0784153fddaf4ae29bcbf5cc6b51c02ba
TheSchoolFriday <20046797@myrp.edu.sg>
2021-07-09 09:52:07 +0800

    finished product

--- app/src/main/java/sg/edu/rp/c346/id20046797/demosimplesave/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20046797/demosimplesave/MainActivity.java
@@ -2,7 +2,9 @@ package sg.edu.rp.c346.id20046797.demosimplesave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +17,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+//        String msg = "No Greeting! (onResume)";
+//        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        String msgSP = prefs.getString("greetings", "Nothing was found.");
+        Toast.makeText(this, msgSP, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
+        // Init the SharedPreference to Save Data
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+
+        // Enter Edit Mode.
+        SharedPreferences.Editor prefEdit = prefs.edit();
+
+        // Putting data inside the SharedPreference called prefEdit
+        prefEdit.putString("greetings", "Hello!");
+        prefEdit.commit();
+
     }
 }
